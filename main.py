50f19612ac0d6662fe0718cb20089a98c23bbbed
TrungNhan26 <thaohiennhan2012@gmail.com>
2025-02-19 23:11:05 +0700

    update main

--- main.py
+++ main.py
@@ -17,5 +17,8 @@ app.add_middleware(
 # Đăng ký API router
 app.include_router(api_router)
 
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
